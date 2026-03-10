12b503a987a734792725023d8dc586380ce332f9
ashishpagare22 <ashishpagare2210@gmail.com>
2026-03-10 23:42:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+async function searchUniversities(){
+
+const country = document.getElementById("searchInput").value;
+
+const response = await fetch(
+`http://universities.hipolabs.com/search?country=${country}`
+);
+
+const data = await response.json();
+
+const resultsDiv = document.getElementById("results");
+
+resultsDiv.innerHTML = "";
+
+data.forEach(university => {
+
+resultsDiv.innerHTML += `
+<div class="card">
+
+<h3>${university.name}</h3>
+
+<p><strong>Country:</strong> ${university.country}</p>
+
+<p>
+<a href="${university.web_pages[0]}" target="_blank">
+Visit Website
+</a>
+</p>
+
+</div>
+`;
+
+});
+
+}
